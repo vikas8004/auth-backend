8f125f418902a1e6954dccc4d46de5fafe980329
vikas <vikas80046@gmail.com>
2024-03-08 05:56:15 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import userRouter from "./routers/user.router.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://auth-vikas-frontend.vercel.app"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use("/api/v1", userRouter);
+
+export default app;
